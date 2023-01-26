686ed9e0d3e7f2fcd7adb730204420bceaab2430
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-26 18:06:25 +0500

    standatization

--- zban_history/init.lua
+++ zban_history/init.lua
@@ -1,8 +1,8 @@
 zban_history = {}
-local hist = core.get_mod_storage()
+local s = core.get_mod_storage()
 
 function zban_history.save(player,text)
-    hist:set_string(player,hist:get_string(player)..text.."\n")
+    s:set_string(player,s:get_string(player)..text.."\n")
 end
 
 core.register_chatcommand("zbanhist",{
@@ -14,9 +14,9 @@ core.register_chatcommand("zbanhist",{
         if not player then return false, "Erro: No Player!" end
         local textonly = param:match(" %-t")
         if textonly then
-            return true, hist:get_string(player)
+            return true, s:get_string(player)
         else
             core.show_formspec(name,"zbanhist","size[16,9]" ..
-"textarea[0.4,0.3;15.7,10;New TextArea;Ban history of "..player..":;"..hist:get_string(player).."]")
+"textarea[0.4,0.3;15.7,10;New TextArea;Ban history of "..player..":;"..s:get_string(player).."]")
         end
 end})
